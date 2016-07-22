de06d85fc7400a491ed52f3a7a27bee67ba0203a
nwf839 <CactusJ43@gmail.com>
2016-07-22 15:36:38 -0400

    Updated user-list UI
    Added buttons to sidebar that link to user photos, and put
    framework in place for future server API updates and new comment
    view

--- components/user-list/user-listController.js
+++ components/user-list/user-listController.js
@@ -1,11 +1,33 @@
 'use strict';
 
-cs142App.controller('UserListController', ['$rootScope', '$scope', 'list', 'updateList',
-    function ($rootScope, $scope, list, updateList) {
+cs142App.controller('UserListController', ['$rootScope', '$scope', '$state', 'list', 'updateList',
+    function ($rootScope, $scope, $state, list, updateList) {
         $scope.main.title = 'Users';
         $scope.main.selectedUser = '';
         $scope.userList = {};
         $scope.userList.users = list;
+
+        // XXX Placeholder models and population method to be used until webServer API
+        // is updated
+        $scope.userList.nPhotos = [];
+        $scope.userList.nComments = [];
+        angular.forEach(list, function() {
+            // XXX May need to convert server data to strings either here or in the
+            // resolve call
+            $scope.userList.nPhotos.push('2');
+            $scope.userList.nComments.push('5');
+        });
+
+        // XXX May need to be changed, but should serve for now
+        $scope.goToUser = function(id) {
+            $state.go('users.detail', {userId: id});
+        };
+        $scope.goToPhotos = function(id) {
+            $state.go('photos.display', {userId: id});
+        };
+        $scope.goToComments = function(id) {
+            console.log('Not Implemented Yet');
+        };
         $rootScope.$on('newUser', updateList($scope.userList.users));
     }
 ]);
