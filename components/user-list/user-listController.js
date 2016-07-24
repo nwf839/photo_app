98d1914682a071784ee2b74d5c6f1279dd56c6f5
nwf839 <CactusJ43@gmail.com>
2016-07-24 18:16:17 -0400

    Updated user-list component to support feature flag
    Added ng-show directives to the icon buttons displaying comment
    and photo counts so that they only appear when advanced features
    are enabled, and deleted placeholder code in the controller

--- components/user-list/user-listController.js
+++ components/user-list/user-listController.js
@@ -7,16 +7,6 @@ cs142App.controller('UserListController', ['$rootScope', '$scope', '$state', 'li
         $scope.userList = {};
         $scope.userList.users = list;
 
-        // XXX May need to be changed, but should serve for now
-        $scope.goToUser = function(id) {
-            $state.go('users.detail', {userId: id});
-        };
-        $scope.goToPhotos = function(id) {
-            $state.go('photos.display', {userId: id});
-        };
-        $scope.goToComments = function(id) {
-            console.log('Not Implemented Yet');
-        };
         $rootScope.$on('newUser', updateList($scope.userList.users));
     }
 ]);
