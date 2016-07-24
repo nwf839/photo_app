b2de0e1f5d950d2f1935449c82fa38eca85395b5
nwf839 <CactusJ43@gmail.com>
2016-07-24 17:40:38 -0400

    Update UserPhotoDetailController
    Added functionality to user-photoDetailController.js so that comments
    would be displayed immediately after they were posted, and made
    requisite changed to user-photoDetailTemplate.html

--- components/user-photos/user-photoDetailController.js
+++ components/user-photos/user-photoDetailController.js
@@ -1,10 +1,22 @@
 'use strict';
 
-cs142App.controller('UserPhotoDetailController', ['$scope', '$stateParams', function($scope, $stateParams) {
+cs142App.controller('UserPhotoDetailController', ['$scope', '$stateParams', 'AddCommentService', function($scope, $stateParams, AddCommentService) {
     var photoId = $stateParams.photoId,
-        index = $scope.userPhotos.ids.indexOf(photoId);
+        index = $scope.userPhotos.ids.indexOf(photoId),
+        replacePhoto = function(photo) {
+            $scope.userPhotoDetail.photo = photo;
+            $scope.userPhotoDetail.comment = '';
+        };
+
     $scope.userPhotoDetail = {};
     $scope.userPhotoDetail.photo = $scope.userPhotos.photos[index];
-    $scope.userPhotoDetail.comment = $scope.userPhotos.comments[index];
-    $scope.userPhotoDetail.addComment = $scope.userPhotos.addComment.bind(null, photoId, index);
+    $scope.userPhotoDetail.commentModel = $scope.userPhotos.comments[index];
+    $scope.userPhotoDetail.addComment = function() {
+        return AddCommentService.addComment(photoId, $scope.userPhotoDetail.commentModel)
+            .then(replacePhoto);
+    };
+
+    $scope.$watch('userPhotoDetail.photo', function(newValue, oldValue) {
+        if (newValue !== oldValue) $scope.userPhotoDetail.photo = newValue;
+    });
 }]);
