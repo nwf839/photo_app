b7e4390e5bd0d4b5e9c6afc44f40d33c5d43badb
nwf839 <CactusJ43@gmail.com>
2016-07-24 16:04:37 -0400

    Added Service
    Added new angular service to frontend that interacts with the
    '/comments/:userId' function of Webserver API

---
+++ services/user-commentService.js
@@ -0,0 +1,13 @@
+services.factory('UserCommentService', ['$resource', function($resource) {
+    var resource = $resource('comments/:userId');
+    var comments = [];
+
+    return {
+        getComments: function(userId) {
+            return resource.get({userId: userId}).$promise
+                .then(function(response) {
+                    return response.toJSON();
+                });
+        }
+    }
+}]);
